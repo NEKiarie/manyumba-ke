e5276386667952d0509f3881a612c365cfd5ded9
Mildred <mildredkoskei01@gmail.com>
2023-01-04 21:48:24 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+properties = Property.create([{
+    seller_id: 1,
+    location: "Kiambu",
+    price: 16000,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzI8lKOFjUtx-SEFtq_M3EA1pM8NOnKxjtKZxGs-cmKxAuZS20uKzYix_OSYPwq5mSEQ0&usqp=CAU",
+    description: ""
+},
+{
+    seller_id: 2,
+    location: "Westlands",
+    price: 30000,
+    image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfCGewHDEpFWJSiJ5rqkbdLdwigyBQXfn6l6kZsMqkQmO9tcSnTnTmT28B3-wBcebRHr0&usqp=CAU" ,
+    description: " "
+},
+{
+    seller_id: 1,
+    location: "Thika Road",
+    price: 17000,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaVLk-jpjwZIaAGzbufINQBQhr6C7knDf6W6eUZr1Qw96pnag9W_3PXL-YmKoy___6lcc&usqp=CAU",
+    description: ""
+},
+{
+    seller_id: 3,
+    location: "Kiambu",
+    price: 16000,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBRoyxz9LoOG3i-Rn6It78sgjKCd6VbrAPfg&usqp=CAU",
+    description: "You’ll find tasteful updates throughout this recently renovated, stunning home. Schedule a tour today!" 
+},
+{
+    seller_id: 4,
+    location: "Kilimani",
+    price: 35000,
+    image_url: "https://www.tysons.co.ke/wp-content/uploads/2019/10/mi-vida-tysons-limited-4.jpg",
+    description: "" 
+},
+{
+    seller_id: 5,
+    location: "Kitengela",
+    price: 24000,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa1FGC98ls-738d2BZmgbcNAkckHOtaLu8pw&usqp=CAU" ,
+    description:"This move-in ready home has been recently updated, including new windows that provide ample of natural light. Enjoy the shaded backyard or walk to the neighborhood park down the street." 
+},
+{
+    seller_id: 2,
+    location: "Ngong Road",
+    price: 20000,
+    image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9ieIPfAzdkRsyQrL4qETVNuVzz3Wi97aO1f6-M6hkwPh3OUXb-i6_5-_oQmLEPHTWAQ&usqp=CAU" ,
+    description: "This stunning two-story home is on a large lot in a hot neighborhood. From the open-concept kitchen and living space to the large shaded backyard, there is plenty of room for the whole family to enjoy. Recent updates include new carpeting upstairs and stainless appliances. Situated in a family-friendly neighborhood near a great park, this home is sure to go fast!"
+}
+
+])
+
+
