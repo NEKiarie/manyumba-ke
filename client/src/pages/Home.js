53dd8b68b08c8baef52e1d0a1511ffb9e4fa685d
+ <+>
2023-01-13 15:02:58 +0300

    Added LoginPage Component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import HouseList from '../components/HouseList';
 
 
 const Home = () => {
-  return <div className='min-h-[1800px]'>
+  return <div className='min-h-[1800px] bg-white'>
+    <Header />
     <Banner />
     <HouseList />
+    <Footer />
   </div>;
 };
 
