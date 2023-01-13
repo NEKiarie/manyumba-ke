49793d00464e259d4ec2792b66e85b6194fd8859
Dennis Njogu <dcolonel6@gmail.com>
2023-01-13 14:37:46 +0300

    formated price to have commas

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -3,5 +3,8 @@ class PropertySerializer < ActiveModel::Serializer
   :size, :image_url, :notes, :fore_closure, :price, :seller_id, :description
 
   belongs_to :location
- 
+  
+  def price
+    self.object.price.to_fs(:delimited)
+  end
 end
