0892a67871987893548878e007a8d5979881cd68
jimbrian <ui.ux.jimbrian@gmail.com>
2023-01-15 11:50:16 +0300

    footer update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return <footer className='bg-black py-8 text-center text-white'>
-    <div className='container mx-auto'>Copyright &copy; 2023. All rights reserved.</div>
+    <div className='container mx-auto'>Manyumba.ke | Copyright  &copy; 2023. All rights reserved.</div>
   </footer>;
 };
 
