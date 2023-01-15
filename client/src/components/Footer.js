efd2ca247fc299dade34e980821d3ce61d11c98e
jimbrian <ui.ux.jimbrian@gmail.com>
2023-01-15 11:56:37 +0300

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,8 @@
 import React from 'react';
 
 const Footer = () => {
-<<<<<<< HEAD
-  return <footer className='bg-black py-8 text-center text-white'>
-    <div className='container mx-auto'>Manyumba.ke | Copyright  &copy; 2023. All rights reserved.</div>
-=======
   return <footer className='bg-violet-800 py-8 text-center text-white'>
     <div className='container mx-auto'>Copyright &copy; 2023. All rights reserved.</div>
->>>>>>> fb3d3e104bfc6e555733800438f4164181172a0a
   </footer>;
 };
 
